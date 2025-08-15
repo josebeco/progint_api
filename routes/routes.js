698bfe77974a4ac8c121c52ecfb45a0a1126c2af
josebeco <jose.p.sambatti@gmail.com>
2025-08-15 09:30:48 -0300

    a

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ import { getAllDados } from "../controllers/controller.js";
 
 const rota = Router();
 
-//rota pra retornar os dados
+
 rota.get('/', getAllDados);
 
 export default rota;
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 import express from "express";
 import dataRoutes from "./routes/routes.js";
-import cors from "cors";
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors);
 
 //Middleware para interpretar o JSON
 app.use(express.json());
