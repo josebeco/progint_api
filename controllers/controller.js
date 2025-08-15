5e52f6d1ce20fd7479e38c9f161eb3dedf01127d
josebeco <jose.p.sambatti@gmail.com>
2025-08-15 09:48:49 -0300

    a

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,5 @@
 import sampleDados from "../data/sampleDados.js";
 
 export const getAllDados = (req, res) => {
-    console.log("Cara se o jao tiver nessa lista de dados eu vou ficar mt puto");
     res.json(sampleDados);
 }
